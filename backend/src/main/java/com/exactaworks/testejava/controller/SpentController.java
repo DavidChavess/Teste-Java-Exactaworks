bf8e17d727926795e0a9109f3bff234383d73ea5
David Chaves <davi.ch.fe@gmail.com>
2020-08-02 19:03:54 -0300

    FEATURE: Implementado documentação com Swagger

--- backend/src/main/java/com/exactaworks/testejava/controller/SpentController.java
+++ backend/src/main/java/com/exactaworks/testejava/controller/SpentController.java
@@ -15,7 +15,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exactaworks.testejava.dto.SpentDto;
+import com.exactaworks.testejava.exception.StandardError;
+import com.exactaworks.testejava.exception.ValidationError;
 import com.exactaworks.testejava.service.SpentService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 
 @RestController
@@ -25,18 +31,23 @@ public class SpentController {
 	@Autowired
 	private SpentService service;
 	
+	@ApiOperation(value = "Retorna todos os gastos")
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
 	public List<SpentDto> findAll(){
 		return service.findAll();
 	}
 	
+	@ApiOperation(value = "Retorna um gasto por id")
+	@ApiResponses(value = @ApiResponse(code = 404, message = "Gasto não encontrado", response = StandardError.class))
 	@GetMapping(value = "/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public SpentDto findById(@PathVariable Long id){
 		return service.findById(id);
 	}	
 	
+	@ApiOperation(value = "Insere um gasto")
+	@ApiResponses(value = @ApiResponse(code = 400, message = "Erro de validação", response = ValidationError.class))
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public SpentDto insert(@RequestBody @Valid SpentDto spentDto) {
