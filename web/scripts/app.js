2976803b5a79a268146e3987768e15d1c224dc7f
David Chaves <davi.ch.fe@gmail.com>
2020-08-03 20:38:34 -0300

    Upload web

---
+++ web/scripts/app.js
@@ -0,0 +1,16 @@
+const btnShow = document.getElementById('show');
+const form = document.forms[0];
+const formSearch = document.forms[1];
+let flagShow = false;
+
+btnShow.addEventListener('click', ()=>{
+   if(flagShow){
+       form.style.display = 'flex';
+       formSearch.style.display = 'none';
+       flagShow = false;
+   }else{
+        form.style.display = 'none';
+        formSearch.style.display = 'block';
+        flagShow = true;
+   }
+})
